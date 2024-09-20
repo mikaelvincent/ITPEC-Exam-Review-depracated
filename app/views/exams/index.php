3fd5c4a6928ab6513725903fe4a6b531cd0bc577
mikaelvincent <tumampos@mikaelvincent.dev>
2024-09-20 20:47:22 +0800

    Replaced placeholder content with static placeholder content in views
    - Updated `header.php`, `home/index.php`, `exams/showQuestion.php`, `exams/showExamSet.php`, `exams/index.php`, and `contributors/index.php` by replacing placeholder content with static placeholder text.

--- app/views/exams/index.php
+++ app/views/exams/index.php
@@ -1,6 +1,13 @@
 <?php require_once __DIR__ . "/../layouts/header.php"; ?>
 
-<h1><?php echo $data["exam_name"]; ?></h1>
-<p>Exam sets will be listed here for <?php echo $data["exam_name"]; ?>.</p>
+    <div class="row row-cols-1">
+        <div class="col mb-3">
+            <a class="btn btn-primary btn-lg h-100 w-100" role="button" href="#">2007Apr_FE_AM</a>
+        </div>
+        <div class="col mb-3">
+            <a class="btn btn-primary btn-lg h-100 w-100" role="button" href="#">2007Oct_FE_AM</a>
+        </div>
+    </div>
+</div>
 
 <?php require_once __DIR__ . "/../layouts/footer.php"; ?>
--- app/views/exams/showExamSet.php
+++ app/views/exams/showExamSet.php
@@ -1,8 +1,31 @@
 <?php require_once __DIR__ . "/../layouts/header.php"; ?>
 
-<h1><?php echo $data["exam_set_name"]; ?></h1>
-<p>Questions for <?php echo $data[
-    "exam_set_name"
-]; ?> will be displayed here.</p>
+    <div class="row">
+        <div class="col-3 mb-3">
+            <a class="btn btn-primary btn-lg h-100 w-100" role="button" href="#">Q1</a>
+        </div>
+        <div class="col-3 mb-3">
+            <a class="btn btn-primary btn-lg h-100 w-100" role="button" href="#">Q2</a>
+        </div>
+        <div class="col-3 mb-3">
+            <a class="btn btn-primary btn-lg h-100 w-100" role="button" href="#">Q3</a>
+        </div>
+        <div class="col-3 mb-3">
+            <a class="btn btn-primary btn-lg h-100 w-100" role="button" href="#">Q4</a>
+        </div>
+        <div class="col-3 mb-3">
+            <a class="btn btn-primary btn-lg h-100 w-100" role="button" href="#">Q77</a>
+        </div>
+        <div class="col-3 mb-3">
+            <a class="btn btn-primary btn-lg h-100 w-100" role="button" href="#">Q78</a>
+        </div>
+        <div class="col-3 mb-3">
+            <a class="btn btn-primary btn-lg h-100 w-100" role="button" href="#">Q79</a>
+        </div>
+        <div class="col-3 mb-3">
+            <a class="btn btn-primary btn-lg h-100 w-100" role="button" href="#">Q80</a>
+        </div>
+    </div>
+</div>
 
 <?php require_once __DIR__ . "/../layouts/footer.php"; ?>
