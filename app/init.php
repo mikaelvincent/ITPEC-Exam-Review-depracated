1e1d9bd5237c56ca064eed086b17b735a14a7b7b
mikaelvincent <tumampos@mikaelvincent.dev>
2024-09-22 20:18:45 +0800

    Refactor autoloading and clean up redundant includes
    Updated `spl_autoload_register` in `app/init.php` to include the `controllers/` directory for dynamic loading. Removed redundant includes in `public/index.php`, as they are now automatically loaded through the autoloader.

--- app/init.php
+++ app/init.php
@@ -3,7 +3,7 @@
 spl_autoload_register(function ($class) {
     $base_dir = __DIR__ . "/";
 
-    $directories = ["core/", "models/"];
+    $directories = ["core/", "models/", "controllers/"];
 
     foreach ($directories as $directory) {
         $file = $base_dir . $directory . $class . ".php";
--- public/index.php
+++ public/index.php
@@ -1,8 +1,6 @@
 <?php
 
 require_once __DIR__ . "/../app/init.php";
-require_once __DIR__ . "/../app/core/App.php";
-require_once __DIR__ . "/../app/core/Controller.php";
 require_once __DIR__ . "/../app/helpers/breadcrumb_helper.php";
 require_once __DIR__ . "/../app/helpers/env_helper.php";
 require_once __DIR__ . "/../app/helpers/url_helper.php";
