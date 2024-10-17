9f5821b061a97ef01c6a2c232807eea2daafcdc9
mikaelvincent.dev <tumampos@mikaelvincent.dev>
2024-10-17 22:13:49 +0800

    Update header layout to align with new design specifications
    - Replaced the existing Bootstrap theme with the 'Lumen' theme to match the updated design aesthetics.

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -3,11 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-        <title><?php echo isset($data["page_title"])
-            ? $data["page_title"] . " | ITPEC Exam Review"
-            : "ITPEC Exam Review"; ?></title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/sketchy/bootstrap.min.css">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Neucha|Cabin+Sketch&amp;display=swap">
+        <title>
+            <?php echo isset($data["page_title"]) ? $data["page_title"] . " | ITPEC Exam Review" : "ITPEC Exam Review";?>
+        </title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.3/dist/lumen/bootstrap.min.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,300;0,400;0,700;1,400&amp;display=swap">
         <?php if (isset($additional_css) && is_array($additional_css)) {
             foreach ($additional_css as $css_file) {
                 echo '<link rel="stylesheet" href="' . $css_file . '">';
